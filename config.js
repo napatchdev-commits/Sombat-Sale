3bd8f1ad59cb29fbe6a351cce3e361307925e5d4
napatchdev-commits <napatch.dev@gmail.com>
2026-07-15 21:08:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 const CONFIG = {
   // วางลิงก์ Google Apps Script Web App URL ของคุณที่นี่
   // เพื่อให้ระบบเชื่อมโยงข้อมูลตรงกันอย่างถาวรในทุกเครื่องและมือถือ โดยที่ข้อมูลจะไม่หายเมื่อคุณล้างแคชเบราว์เซอร์
-  API_URL: "https://script.google.com/macros/s/AKfycbyMOgEdwmLGvQJvvkzOh2sCDNKnuFxJPC_4QJn5YtYhI2JsZKMTWOS2Of_mdGXSnJHJmA/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbzxtwO0wifGwyw9TQ9mwmYKyfKeS15Q9SvQUCXlFNQrCfFoTWVZb2I0JWtRnjpU8gb0/exec"
   };
